ac604fde5b1bbd7ec2e08d4316080b93c4b5aeb9
goli-arch <cc2799438885@gmail.com>
2026-06-05 19:02:24 +0800

    feat: Hello World - Python & C

---
+++ hello.c
@@ -0,0 +1,7 @@
+﻿#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    printf("   - Codex & goli-arch\n");
+    return 0;
+}
---
+++ hello.py
@@ -0,0 +1,7 @@
+﻿def main():
+    print("Hello, World!")
+    print("   - Codex")
+    print("   - goli-arch")
+
+if __name__ == "__main__":
+    main()
