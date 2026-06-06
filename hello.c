5ab62071128fff69f7597bf376049788dad520db
goli-arch <cc2799438885@gmail.com>
2026-06-06 09:50:40 +0800

    chore: 删除 hello.c 和 hello.py

--- hello.c
+++
@@ -1,7 +0,0 @@
-﻿#include <stdio.h>
-
-int main() {
-    printf("Hello, World!\n");
-    printf("   - Codex & goli-arch\n");
-    return 0;
-}
--- hello.py
+++
@@ -1,7 +0,0 @@
-﻿def main():
-    print("Hello, World!")
-    print("   - Codex")
-    print("   - goli-arch")
-
-if __name__ == "__main__":
-    main()
